e1e281645f19c664447ed928b0422d53396b92e7
Luke Driscoll <ldriscoll@chapman.edu>
2023-03-09 18:31:27 -0800

    Formatting
    -Adding Pages, and formatted some elements

---
+++ script.js
@@ -0,0 +1,13 @@
+//List of project objects that have a Title, description, image, and link
+var projects = [
+    {
+        title: "Project 1",
+        description: "This is a project",
+        image: "images/197x148.gif",
+        link: "http://www.google.com"
+    }
+];
+
+
+//Method for changing the projects
+//Method to send the form
